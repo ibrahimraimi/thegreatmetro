a6303cdabcc68fef5077490760ff950b89dfb571
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-07-19 17:51:28 +0100

    feat: add initial configuration and utilities
    - Created postcss.config.js for PostCSS setup with Tailwind CSS and autoprefixer.
    - Added favicon.ico for the application.
    - Included CalSans-SemiBold font in the public/fonts directory.
    - Initialized rome.json for linter and formatter configuration.
    - Set up tailwind.config.js with custom theme, typography, animations, and plugins.
    - Established tsconfig.json for TypeScript configuration with Next.js support.
    - Implemented useMousePosition hook to track mouse coordinates.

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "thegreatmetro.xyz",
+    template: "%s | thegreatmetro.xyz",
+  },
+  description:
+    "The personal website of thegreatmetro, a software engineer and open source enthusiast.",
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <body
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
